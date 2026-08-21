3b9884d913a079186b178758533c07d9ca36194d
Patcharapon Oonjan <example@email.com>
2026-08-21 14:55:29 +0700

    v.2-Template-6804101361

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,23 @@
 <template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px">
-      <q-input v-model="name" filled label="Your name *" hint="Name and surname" />
+      <q-input
+        v-model="name"
+        filled
+        label="Your name *"
+        hint="Name and surname"
+      />
       <q-input v-model="age" filled type="number" label="Your age *" />
       <q-toggle v-model="accept" label="I accept the license and terms" />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
-        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn
+          label="RESET"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
       </div>
     </q-form>
   </q-page>
@@ -19,4 +30,3 @@ const name = ref(null)
 const age = ref(null)
 const accept = ref(false)
 </script>
-
